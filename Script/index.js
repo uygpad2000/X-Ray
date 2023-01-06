d58df4020a397fb265a0ae23c757e76e97ced01c
uygpad2000 <121656455+uygpad2000@users.noreply.github.com>
2023-01-06 22:50:23 +0800

    Create index.js

---
+++ Script/index.js
@@ -0,0 +1,6 @@
+CreateElement('html','head')
+let title = CreateElement('head','title')
+title.textContent = "My Blog"
+CreateElement('html','body')
+node0 = CreateElement('body','h1')
+node0.textContent = "Welcome!"
