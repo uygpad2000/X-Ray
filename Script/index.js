68119ccf8f751477bba6e993137477b4960205ac
uygpad2000 <121656455+uygpad2000@users.noreply.github.com>
2023-01-09 19:03:53 +0800

    Update index.js

--- Script/index.js
+++ Script/index.js
@@ -1,10 +1,1 @@
-title = Create('head','title')
-title.textContent = "My Blog"
-meta = Create('head','meta')
-Set_Element_Attributes(meta,'charset','utf-8')
-link0 = Create('head','link')
-Set_Element_Attributes(link0,'rel','stylesheet')
-Set_Element_Attributes(link0,'type','text/css')
-Set_Element_Attributes(link0,'href','Style/Page.css')
-h1 = Create('body','h1')
-h1.textContent = "Welcome!"
+Create('html','body');
