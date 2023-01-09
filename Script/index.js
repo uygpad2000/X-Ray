9b47cc9aba6ff5231785d5bbc007ad124c7eba03
uygpad2000 <121656455+uygpad2000@users.noreply.github.com>
2023-01-09 14:06:46 +0800

    Update index.js

--- Script/index.js
+++ Script/index.js
@@ -6,6 +6,5 @@ var link0 = Create('head','link')
 Set_Element_Attributes(link0,'rel','stylesheet')
 Set_Element_Attributes(link0,'type','text/css')
 Set_Element_Attributes(link0,'href','Style/Page.css')
-Create('html','body')
 var node0 = Create('body','h1')
 node0.textContent = "Welcome!"
