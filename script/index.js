af896eb5e20e3695a740f716dc965d20f912024d
Uygpad2000 <121656455+uygpad2000@users.noreply.github.com>
2023-03-16 22:25:58 +0800

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,21 +1,12 @@
-/* 
-    javascript for index page
-*/
-
 var UISet = [
-    {'Tag':'title','Parent':'head','Text':'My Blog'},
-    {'Tag':'h1','Text':'Directory'},
-    {'Tag':'div','Text':'about','id':'about'},
-    {'Tag':'link','Parent':'head','rel':'stylesheet',
-    'href':'.\\style\\page.css'}
-]
-Create(UISet)
-bindEvent('div',showInfo,0)
-
-function showInfo(){
-    
-    alert(
-        'Made by Uygpad2000,for more information please visit:\n'+
-        '               https//github.com/uygpad2000')
-
-}
+// Page Info
+{'Tag':'title','Text':'Uygpad2000'},
+{'Tag':'link','rel':'icon','href':'.\\res\\ico.ico','Parent':'head'},
+// Load Stylesheet
+{'Tag':'link','rel':'stylesheet','href':'.\\style\\index.css','Parent':'head'},
+// DOM Element
+{'Tag':'h1','Text':'Uygpad\'s Home'},
+{'Tag':'div','Text':'Site Directory'},
+{'Tag':'div','Text':'Sorry,but this site is under construction','id':'warn'}
+            ]
+Generate(UISet)
