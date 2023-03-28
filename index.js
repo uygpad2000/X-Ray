0df9e328d8dd8cc7ad2f1dfa00c816fe1c7a09f4
Uygpad2000 <121656455+uygpad2000@users.noreply.github.com>
2023-03-28 18:33:10 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import { xDevice } from './lib/xDevice/xDevice.js'
+import { xDevice } from './lib/XDevice/Xdevice.js'
 var token =
 [
 {'tag':'title','parent':'head','pindex':0,'text':'index'},
 {'tag':'h1','parent':'body','pindex':0,'text':'X-Ray Framework'}
 ]
 var device = new xDevice()
-token.push({'tag':'h1','parent':'body','pindex':0,'text':'xDevice.js:'+device.UA})
+token.push({'tag':'h2','parent':'body','pindex':0,'text':'xDevice.js:'+device.UA})
+token.push({'tag':'h3','parent':'body','pindex':0,'text':'Lang:'+device.lang})
+token.push({'tag':'h4','parent':'body','pindex':0,'text':'System:'+device.os})
 Create(token)
