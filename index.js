b8e7c3d5c3f757ad2a3b75b01f792c12c26b83fb
Uygpad2000 <121656455+uygpad2000@users.noreply.github.com>
2023-03-31 00:48:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-import * as xray from '.\lib\X-Ray.js'
 var token =
 [
 {'tag':'title','parent':'head','pindex':0,'text':'index'},
 {'tag':'h1','parent':'body','pindex':0,'text':'X-Ray Framework'},
 {'tag':'link','parent':'head','pindex':0}
 ]
-xray.Create(token)
+Create(token)
