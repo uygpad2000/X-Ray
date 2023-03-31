2aee9054c84292da486e2282b383b23dd420c0a0
Uygpad2000 <121656455+uygpad2000@users.noreply.github.com>
2023-03-31 19:21:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var token =
 {'tag':'title','parent':'head','pindex':0,'text':'index'},
 {'tag':'h1','parent':'body','pindex':0,'text':'X-Ray Framework'},
 {'tag':'link','parent':'head','pindex':0,'rel':'stylesheet','href':'./style/style.css'},
-{'tag':'img','parent':'body','pindex':0,'src':'./res/Title.png','width':'150px','border-radius':'10px'}
+{'tag':'img','parent':'body','pindex':0,'src':'./res/Title.png','width':'150px','border-radius':'10px','margin-left':'5px'}
 ]
 xray.Create(token)
