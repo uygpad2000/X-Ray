5ad1d0a9ef8c5b7b49c3c5ec1812a657bb332606
Uygpad2000 <121656455+uygpad2000@users.noreply.github.com>
2023-03-31 21:24:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ var token =
 {'tag':'img','parent':'body','pindex':0,'src':'./res/Title.png','width':'150px','border-radius':'10px','margin-left':'5px'}
 ]
 xray.Create(token)
+var device = xray.getDeviceType()
+var token = [{'tag':'div','parent':'body','pindex':0,'text':'platform:'+device.platform}]
+xray.Create(token)
