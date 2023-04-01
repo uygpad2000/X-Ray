30350e3d705cb5b5c5e8ae9eba14aef5eb647277
Uygpad2000 <121656455+uygpad2000@users.noreply.github.com>
2023-04-01 19:02:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ var token =
 {'tag':'title','parent':'head','pindex':0,'text':'index'},
 {'tag':'h1','parent':'body','pindex':0,'text':'X-Ray Framework'},
 {'tag':'link','parent':'head','pindex':0,'rel':'stylesheet','href':'./style/style.css'},
-{'tag':'img','parent':'body','pindex':0,'src':'./res/Title.png','id':'x-ray'}
+{'tag':'img','parent':'body','pindex':0,'src':'./res/Title.png','id':'x-ray'},
+{'tag':'div','parent':'body','pindex':0,'text':'Machine Configuration:'}
 ]
 xray.Create(token)
 var device = xray.getDeviceType()
 var platform = device.platform
-var token = [{'tag':'div','parent':'body','pindex':0,'text':'platform:'+device.platform+'('+navigator.platform+')','id':'platform-info'},
+var token = [{'tag':'div','parent':'body','pindex':0,'text':'Platform:'+device.platform+'('+navigator.platform+')','id':'platform-info'},
              {'tag':'div','parent':'body','pindex':0,'text':'Screen Resolution:'+device.resolution[0]+'x'+device.resolution[1]},
              {'tag':'link','parent':'head','pindex':0,'rel':'icon','href':'./res/icon.ico'}]
 xray.Create(token)
-device.adjustResolution('640px','480px')
+// device.adjustResolution('640px','480px')
