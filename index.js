7e84654144878bf5345d85ffafc0a0e3178329d8
Uygpad2000 <121656455+uygpad2000@users.noreply.github.com>
2023-04-02 00:23:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ var token = [{'tag':'div','parent':'body','pindex':0,'text':'Platform:'+device.p
              {'tag':'link','parent':'head','pindex':0,'rel':'icon','href':'./res/icon.ico'},
              {'tag':'button','parent':'body','pindex':0,'text':'Language Setting'}]
 var handler = xray.Create(token)
-console.log(handler[3])
+xray.createAttributes(handler[3],['onclick','javascript:window.alert("すみません，分かりません")'])
 // device.adjustResolution('640px','480px')
