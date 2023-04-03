0db4de7f8ba45bce47f2560b04ea2b9c06625a35
Uygpad2000 <121656455+uygpad2000@users.noreply.github.com>
2023-04-03 22:28:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import * as xray from './lib/X-Ray.js'
 
 var token =
 [
-{'tag':'title','parent':'head','pindex':0,'text':'index'},
+{'tag':'title','parent':'head','pindex':0,'text':'X-Ray Framework'},
 {'tag':'h1','parent':'body','pindex':0,'text':'X-Ray Framework'},
 {'tag':'link','parent':'head','pindex':0,'rel':'stylesheet','href':'./style/style.css'},
 {'tag':'img','parent':'body','pindex':0,'src':'./res/Title.png','id':'x-ray'},
